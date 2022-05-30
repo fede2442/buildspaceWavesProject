added031279424a522bf698ec0e2afcf956b269b
fede2442 <fede.sarquis@hotmail.com>
2022-05-29 23:46:22 -0300

    connect to metamask implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,74 @@
-import * as React from "react";
+import React, { useEffect, useState} from "react";
 import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
 
-  // const checkWalletConnected = () => {
-  //   const { ethereum } = window;
+  const [currentAccount, setCurrentAccount] = useState("");
 
-  //   if(!ethereum){
-  //     console.log("Download Metamask!");
-  //   }else{
-  //     console.log("We have metamask!", ethereum);
-  //   }
-  // }
+  const checkIfWalletIsConnected = async () => {
+    try{
+      const { ethereum } = window;
 
-  // useEffect(() => {
-  //     checkIfWalletIsConnected();
-  // }, [])
+      if(!ethereum){
+        console.log("Download Metamask!");
+      }else{
+        console.log("We have metamask!", ethereum);
+      }
 
+      const accounts = await ethereum.request({ method: "eth_accounts" });
 
-  const wave = () => {
-    console.log("test");
+      if(accounts.length !== 0){
+        const account = accounts[0];
+        console.log("Found an authorized account: ", account);
+        setCurrentAccount(account);
+      }else{
+        console.log("No authorized account found");
+      }
+
+    }catch (error){
+      console.log("error at getting account", error)
+    }
   }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+      checkIfWalletIsConnected();
+  }, [])
   
   return (
     <div className="mainContainer">
 
       <div className="dataContainer">
         <div className="header">
-            Wave a Friend and maybe win...
+            Connected account: {
+              currentAccount
+            }
         </div>
 
         <div className="bio">
           Hey my name is Fede :) try and wave me, you might get a surprise!
         </div>
 
-        <button className="waveButton" onClick={wave}>
-          Wave 
+        <button className="waveButton" onClick={connectWallet}>
+          Connect Wallet 
         </button>
       </div>
     </div>
