649f215f78a172611cfc432db7651298f4fda0d7
fede2442 <fede.sarquis@hotmail.com>
2022-05-31 02:54:35 -0300

    changed the whole smart contract to do a lotterish game

--- contracts/ProfitThePonzi.sol
+++ contracts/ProfitThePonzi.sol
@@ -10,19 +10,25 @@ import "hardhat/console.sol";
 contract ProfitThePonzi {
     // https://docs.openzeppelin.com/contracts/3.x/api/utils#Counters
     using Counters for Counters.Counter;
-
-    //winner address => amountWon , save winners until they withdraw to follow withdrawal pattern.
+    
+    // Too many variables? think of a struct that could help ?
+    mapping(address => uint256[]) public lotteryOwners;
+    address[] public lotteryPlayers;
+    mapping(uint256 => address) public lotteryTickets;
     mapping(address => uint256) public pendingWinners; 
-    mapping(address => HistoricPlays) public records;
     uint256 public jackpot;
+    uint256 public superJackpot;
+    uint256 public constant amountOfTickets = 10;
+    Counters.Counter public soldTicketsCounter;  
 
+/*
     struct HistoricPlays {
-      Counters.Counter timesPlayed;
+      Counters.Counter ticketsBought;
       Counters.Counter timesWon;
       uint256 totalWon;
       uint256 totalBet;
     }
-
+*/
     modifier onlyWinners()
     {
         // if 0, means that the address is not in the winners map
@@ -31,45 +37,90 @@ contract ProfitThePonzi {
             revert AccountIsNotAWinner();
         _;
     }
-
-    error InvalidNumber();
+    
+    error LotteryNumberAlreadySold();
+    error InvalidTicketNumber();
     error NotTheOwner();
     error AccountIsNotAWinner();
     error AmountUnderMinBet();
+    error MaxAmountOfTickets();
 
-    event Guess(address indexed guesser, uint indexed guessed, uint256 indexed toGuess, uint256 vaultAmount);
     event Winner(address indexed winner, uint256 indexed vaultAmount);
     event Withdrawal(address indexed winner, uint256 indexed amountWitdrawed);
+    event BoughtTicket(address indexed buyer, uint256 indexed ticketNumber);
 
-    // checks - effects - interactions
-    function waveAndGuess(uint256 _guess) public payable returns(bool isWinner){
-        if(msg.value < 0.05 ether) revert AmountUnderMinBet();
-        if(_guess <= 0 || _guess > 10) revert InvalidNumber();
+    function buyTicket(uint256 _ticketNumber) public payable{
+        if(lotteryTickets[_ticketNumber] != address(0)) revert LotteryNumberAlreadySold();
+        if(lotteryOwners[msg.sender].length == 5) revert MaxAmountOfTickets();
+        if(msg.value != 1 ether) revert AmountUnderMinBet();
+        if(_ticketNumber <= 0 || _ticketNumber > amountOfTickets) revert InvalidTicketNumber();
 
-        // decide if winner or loser
-        uint256 numberToGuess = getRandomToGuess();
-        isWinner = numberToGuess == _guess;
-        jackpot = jackpot + msg.value;
+        jackpot += msg.value;
+        lotteryOwners[msg.sender].push(_ticketNumber);
+        lotteryTickets[_ticketNumber] = msg.sender;
+        lotteryPlayers.push(msg.sender);
+        soldTicketsCounter.increment();
 
-        // TODO: Investigate and use Open Zeppelin counters ?
         // received correct bet with enough money and valid number
-        records[msg.sender].timesPlayed.increment(); 
-        records[msg.sender].totalBet = records[msg.sender].totalBet + msg.value; 
+        //records[msg.sender].ticketsBought.increment(); 
+        //records[msg.sender].totalBet += msg.value;
 
-        if(isWinner) setWinner(); 
-        //if(pickLotteryTicket) setLotteryWinner();
+        if(soldTicketsCounter.current() == amountOfTickets){
+            endLottery();
+        }
 
-        emit Guess(msg.sender, _guess, numberToGuess, jackpot);
+        emit BoughtTicket(msg.sender, _ticketNumber);
     }
 
-    // Reset jackpot after new winner to start again
-    function setWinner() internal {
-        records[msg.sender].timesWon.increment();
-        records[msg.sender].totalWon = records[msg.sender].totalWon + jackpot; 
-        pendingWinners[msg.sender] = pendingWinners[msg.sender] + jackpot; //in case someone wins more than once
+    function endLottery() internal {
+        uint256 winnerOne = getRandomToGuess();
+        uint256 winnerTwo = getRandomToGuess() + 1;
+        uint256 winnerThree = getRandomToGuess() + 2;
+
+        address[3] memory winners = [lotteryTickets[winnerOne], lotteryTickets[winnerTwo], lotteryTickets[winnerThree]];
+
+        pendingWinners[winners[0]] += (jackpot * 24 / 50); // 48%
+        pendingWinners[winners[1]] += (jackpot * 13 / 50); // 26%
+        pendingWinners[winners[2]] += (jackpot * 4 / 25); // 16%
+        superJackpot = jackpot / 10;
+
+        resetLottery();
         
+        emit Winner(winners[0], pendingWinners[winners[0]]);
+        emit Winner(winners[1], pendingWinners[winners[1]]);
+        emit Winner(winners[2], pendingWinners[winners[2]]);
+    }
+
+    function resetLottery() internal {
         jackpot = 0;
-        emit Winner(msg.sender, pendingWinners[msg.sender]);
+
+        for(uint i = 0; i <  lotteryPlayers.length; i++){
+            delete lotteryOwners[lotteryPlayers[i]];
+        }
+
+        for(uint i = 1; i <=  amountOfTickets; i++){
+            lotteryTickets[i] = address(0);
+        }
+
+        soldTicketsCounter.reset();
+
+        delete lotteryPlayers;
+    }
+
+    function ticketsBoughtBy(address _buyer) public view returns(uint256[] memory){
+        return lotteryOwners[_buyer];
+    }
+
+    function buyerOfTicket(uint256 _ticketNumber) public view returns(address){
+        return lotteryTickets[_ticketNumber];
+    }
+
+    function soldTickets() public view returns(uint256[amountOfTickets] memory _soldTickets){
+        for(uint i = 0; i <  amountOfTickets; i++){
+            if(lotteryTickets[i] != address(0)){
+                _soldTickets[i] = i + 1;
+            }
+        }
     }
 
     // Using withdrawal pattern to avoid exploits.
@@ -81,37 +132,29 @@ contract ProfitThePonzi {
         //need to retrieve only what they won.
         uint256 totalWon = pendingWinners[msg.sender];
 
-        // restart their claiming price
+        // restart their claiming price (effect)
         pendingWinners[msg.sender] = 0;
+        // interaction
         (bool success, ) = payable(msg.sender).call{value: totalWon}("");
         
         //IMPORTANT: always have a require after ".call" 
         //https://medium.com/coinmonks/solidity-transfer-vs-send-vs-call-function-64c92cfc878a
         require(success, "Withdrawal failed.");
-        emit Withdrawal(msg.sender, totalWon);
-    }
 
-    function getTotalVault() public view returns (uint256){
-        return address(this).balance;
+        emit Withdrawal(msg.sender, totalWon);
     }
 
     function getJackpot() public view returns(uint256){
         return jackpot;
     }
 
-    // TODO: Add a lottery extra fund
-    // function pickLotteryTicket(uint256) public view returns(bool){
-    //     uint8 toGuess = 24;
-    //     uint256 randomNormalized = (getRandomToGuess() * 120) % 100;
-
-    //     console.log("randomNormalized",randomNormalized);
-
-    //     return toGuess == randomNormalized;
-    // } 
-
-    // Modify later on to get a real random number. Possibly use chainlink VRF. Now it's a pseudorandom.
+    // TODO: Modify later on to get a real random number. Possibly use chainlink VRF. Now it's a pseudorandom.
     function getRandomToGuess() internal pure returns (uint256){
         //return (block.difficulty / block.number) % 10;
         return 5;
+    }
+
+    function getBalance() public view returns(uint256){
+        return address(this).balance;
     }
 }
