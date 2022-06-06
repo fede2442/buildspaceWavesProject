93ff7a7bff7a6296ae73388f9c27757ef81fc273
fede2442 <fede.sarquis@hotmail.com>
2022-06-05 23:11:01 -0300

    added tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { BigNumber, ethers } from 'ethers';
-import './App.css';
+import { BigNumber, Contract, ethers } from 'ethers';
 import { abi } from './utils/ProfitThePonzi.json';
+
 
 export default function App() {
   const [currentAccount, setCurrentAccount] = useState('');
@@ -47,6 +47,18 @@ export default function App() {
     }
   };
 
+  const handleClick = async (ticketNumber: number) => {
+    const { ethereum } = window as any;
+    alert(`String text ${ticketNumber}`);
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const lotteryContract: Contract = new ethers.Contract('0x0856aec2139533B25B19F7DC08130A46f650C82e', abi, signer);
+
+    const ticketsSold = await lotteryContract.buyTicket(ticketNumber);
+    await ticketsSold.wait();
+    alert("Ticket number sold");
+  };
+
   useEffect(() => {
     const checkIfWalletIsConnected = async () => {
       try {
@@ -75,15 +87,11 @@ export default function App() {
     checkIfWalletIsConnected();
   }, []);
 
-  function handleClick(ticketNumber: number){
-    alert(`String text ${ticketNumber}`);
-  }
-
   return (
-    <div className="mainContainer">
-      <div className="dataContainer">
-        <div className="header">
-          Connected account:
+    <div className="flex justify-center w-full mt-16">
+      <div className="flex flex-col justify-center">
+        <div className="text-center text-3xl font-semibold">
+          Pick a ticket number:
           {' '}
           {
               currentAccount
@@ -91,43 +99,17 @@ export default function App() {
         </div>
 
         <div className="grid-container">
-          <div className="box" onClick={() => handleClick(1)}>1</div>
-          <div className="box" onClick={() => handleClick(2)}>2</div>
-          <div className="box" onClick={() => handleClick(3)}>3</div>
-          <div className="box" onClick={() => handleClick(3)}>3</div>
-          <div className="box" onClick={() => handleClick(3)}>3</div>
-          <div className="box" onClick={() => handleClick(3)}>3</div>
-          <div className="box" onClick={() => handleClick(3)}>3</div>
-          <div className="box" onClick={() => handleClick(3)}>3</div>
-          <div className="box" onClick={() => handleClick(3)}>3</div>
-          <div className="box" onClick={() => handleClick(3)}>3</div>
-          <div className="box" onClick={() => handleClick(3)}>3</div>
-          <div className="box" onClick={() => handleClick(3)}>3</div>
-          <div className="box" onClick={() => handleClick(3)}>3</div>
+          {Array.from(Array(50)).map((_, index) => (<div className="text-center rounded h-full w-full" onClick={() => handleClick(index)}>{index}</div>))}
         </div>
 
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {Array.from(Array(6)).map((_, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <Item>xs=2</Item>
-            </Grid>
-          ))}
-        </Grid>
-
-
-        <div>
-        Sold Tickets:
-        </div>
-        <ul>{soldTickets.map((x: BigNumber) => <li>{x.toString()}</li>)}</ul>
-
-        <div className="bio">
+        <div className="text-center text-gray-600 mt-4">
           Hey my name is Fede :) try and wave me, you might get a surprise!
         </div>
 
-        <button className="waveButton" onClick={connectWallet}>
+        <button className="cursor-pointer mt-4 p-2 rounded" onClick={connectWallet}>
           Connect Wallet
         </button>
-        <button className="waveButton" onClick={getTicketsSold}>
+        <button className="cursor-pointer mt-4 p-2 rounded" onClick={getTicketsSold}>
           get tickets sold
         </button>
       </div>
